2f1cec9f249d9ba6e813796d173d5e0b38a42442
Restyled.io <commits@restyled.io>
2023-09-26 15:09:02 +0000

    Restyled by clang-format

--- engine.js
+++ engine.js
@@ -1,35 +1,32 @@
-(function () {
-  var lastTime = 0;
-  var vendors = ["ms", "moz", "webkit", "o"];
-  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[vendors[x] + "RequestAnimationFrame"];
-    window.cancelAnimationFrame =
+(function() {
+var lastTime = 0;
+var vendors = [ "ms", "moz", "webkit", "o" ];
+for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+  window.requestAnimationFrame = window[vendors[x] + "RequestAnimationFrame"];
+  window.cancelAnimationFrame =
       window[vendors[x] + "CancelAnimationFrame"] ||
       window[vendors[x] + "CancelRequestAnimationFrame"];
-  }
+}
 
-  if (!window.requestAnimationFrame)
-    window.requestAnimationFrame = function (callback, element) {
-      var currTime = new Date().getTime();
-      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-      var id = window.setTimeout(function () {
-        callback(currTime + timeToCall);
-      }, timeToCall);
-      lastTime = currTime + timeToCall;
-      return id;
-    };
+if (!window.requestAnimationFrame)
+  window.requestAnimationFrame = function(callback, element) {
+    var currTime = new Date().getTime();
+    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+    var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                               timeToCall);
+    lastTime = currTime + timeToCall;
+    return id;
+  };
 
-  if (!window.cancelAnimationFrame)
-    window.cancelAnimationFrame = function (id) {
-      clearTimeout(id);
-    };
+if (!window.cancelAnimationFrame)
+  window.cancelAnimationFrame = function(id) { clearTimeout(id); };
 })();
 
-var Game = new (function () {
+var Game = new (function() {
   var boards = [];
 
   // Game Initialization
-  this.initialize = function (canvasElementId, sprite_data, callback) {
+  this.initialize = function(canvasElementId, sprite_data, callback) {
     this.canvas = document.getElementById(canvasElementId);
 
     this.playerOffset = 10;
@@ -56,37 +53,29 @@ var Game = new (function () {
   };
 
   // Handle Input
-  var KEY_CODES = { 37: "left", 39: "right", 32: "fire" };
+  var KEY_CODES = {37 : "left", 39 : "right", 32 : "fire"};
   this.keys = {};
 
-  this.setupInput = function () {
-    window.addEventListener(
-      "keydown",
-      function (e) {
-        if (KEY_CODES[e.keyCode]) {
-          Game.keys[KEY_CODES[e.keyCode]] = true;
-          e.preventDefault();
-        }
-      },
-      false
-    );
+  this.setupInput = function() {
+    window.addEventListener("keydown", function(e) {
+      if (KEY_CODES[e.keyCode]) {
+        Game.keys[KEY_CODES[e.keyCode]] = true;
+        e.preventDefault();
+      }
+    }, false);
 
-    window.addEventListener(
-      "keyup",
-      function (e) {
-        if (KEY_CODES[e.keyCode]) {
-          Game.keys[KEY_CODES[e.keyCode]] = false;
-          e.preventDefault();
-        }
-      },
-      false
-    );
+    window.addEventListener("keyup", function(e) {
+      if (KEY_CODES[e.keyCode]) {
+        Game.keys[KEY_CODES[e.keyCode]] = false;
+        e.preventDefault();
+      }
+    }, false);
   };
 
   var lastTime = new Date().getTime();
   var maxTime = 1 / 30;
   // Game Loop
-  this.loop = function () {
+  this.loop = function() {
     var curTime = new Date().getTime();
     requestAnimationFrame(Game.loop);
     var dt = (curTime - lastTime) / 1000;
@@ -104,15 +93,12 @@ var Game = new (function () {
   };
 
   // Change an active game board
-  this.setBoard = function (num, board) {
-    boards[num] = board;
-  };
+  this.setBoard = function(num, board) { boards[num] = board; };
 
-  this.setupMobile = function () {
+  this.setupMobile = function() {
     var container = document.getElementById("container"),
-      hasTouch = !!("ontouchstart" in window),
-      w = window.innerWidth,
-      h = window.innerHeight;
+        hasTouch = !!("ontouchstart" in window), w = window.innerWidth,
+        h = window.innerHeight;
 
     if (hasTouch) {
       this.mobile = true;
@@ -153,30 +139,22 @@ var Game = new (function () {
   };
 })();
 
-var SpriteSheet = new (function () {
+var SpriteSheet = new (function() {
   this.map = {};
 
-  this.load = function (spriteData, callback) {
+  this.load = function(spriteData, callback) {
     this.map = spriteData;
     this.image = new Image();
     this.image.onload = callback;
     this.image.src = "images/sprites.png";
   };
 
-  this.draw = function (ctx, sprite, x, y, frame) {
+  this.draw = function(ctx, sprite, x, y, frame) {
     var s = this.map[sprite];
-    if (!frame) frame = 0;
-    ctx.drawImage(
-      this.image,
-      s.sx + frame * s.w,
-      s.sy,
-      s.w,
-      s.h,
-      Math.floor(x),
-      Math.floor(y),
-      s.w,
-      s.h
-    );
+    if (!frame)
+      frame = 0;
+    ctx.drawImage(this.image, s.sx + frame * s.w, s.sy, s.w, s.h, Math.floor(x),
+                  Math.floor(y), s.w, s.h);
   };
 
   return this;
@@ -184,12 +162,14 @@ var SpriteSheet = new (function () {
 
 var TitleScreen = function TitleScreen(title, subtitle, callback) {
   var up = false;
-  this.step = function (dt) {
-    if (!Game.keys["fire"]) up = true;
-    if (up && Game.keys["fire"] && callback) callback();
+  this.step = function(dt) {
+    if (!Game.keys["fire"])
+      up = true;
+    if (up && Game.keys["fire"] && callback)
+      callback();
   };
 
-  this.draw = function (ctx) {
+  this.draw = function(ctx) {
     ctx.fillStyle = "#00FF00";
 
     ctx.font = "bold 40px bangers";
@@ -198,15 +178,12 @@ var TitleScreen = function TitleScreen(title, subtitle, callback) {
 
     ctx.font = "bold 20px bangers";
     var measure2 = ctx.measureText(subtitle);
-    ctx.fillText(
-      subtitle,
-      Game.width / 2 - measure2.width / 2,
-      Game.height / 2 + 40
-    );
+    ctx.fillText(subtitle, Game.width / 2 - measure2.width / 2,
+                 Game.height / 2 + 40);
   };
 };
 
-var GameBoard = function () {
+var GameBoard = function() {
   var board = this;
 
   // The current list of objects
@@ -214,7 +191,7 @@ var GameBoard = function () {
   this.cnt = {};
 
   // Add a new object to the object list
-  this.add = function (obj) {
+  this.add = function(obj) {
     obj.board = this;
     this.objects.push(obj);
     this.cnt[obj.type] = (this.cnt[obj.type] || 0) + 1;
@@ -222,7 +199,7 @@ var GameBoard = function () {
   };
 
   // Mark an object for removal
-  this.remove = function (obj) {
+  this.remove = function(obj) {
     var idx = this.removed.indexOf(obj);
     if (idx == -1) {
       this.removed.push(obj);
@@ -233,12 +210,10 @@ var GameBoard = function () {
   };
 
   // Reset the list of removed objects
-  this.resetRemoved = function () {
-    this.removed = [];
-  };
+  this.resetRemoved = function() { this.removed = []; };
 
   // Removed an objects marked for removal from the list
-  this.finalizeRemoved = function () {
+  this.finalizeRemoved = function() {
     for (var i = 0, len = this.removed.length; i < len; i++) {
       var idx = this.objects.indexOf(this.removed[i]);
       if (idx != -1) {
@@ -249,7 +224,7 @@ var GameBoard = function () {
   };
 
   // Call the same method on all current objects
-  this.iterate = function (funcName) {
+  this.iterate = function(funcName) {
     var args = Array.prototype.slice.call(arguments, 1);
     for (var i = 0, len = this.objects.length; i < len; i++) {
       var obj = this.objects[i];
@@ -258,41 +233,36 @@ var GameBoard = function () {
   };
 
   // Find the first object for which func is true
-  this.detect = function (func) {
+  this.detect = function(func) {
     for (var i = 0, val = null, len = this.objects.length; i < len; i++) {
-      if (func.call(this.objects[i])) return this.objects[i];
+      if (func.call(this.objects[i]))
+        return this.objects[i];
     }
     return false;
   };
 
   // Call step on all objects and them delete
   // any object that have been marked for removal
-  this.step = function (dt) {
+  this.step = function(dt) {
     this.resetRemoved();
     this.iterate("step", dt);
     this.finalizeRemoved();
   };
 
   // Draw all the objects
-  this.draw = function (ctx) {
-    this.iterate("draw", ctx);
-  };
+  this.draw = function(ctx) { this.iterate("draw", ctx); };
 
   // Check for a collision between the
   // bounding rects of two objects
-  this.overlap = function (o1, o2) {
-    return !(
-      o1.y + o1.h - 1 < o2.y ||
-      o1.y > o2.y + o2.h - 1 ||
-      o1.x + o1.w - 1 < o2.x ||
-      o1.x > o2.x + o2.w - 1
-    );
+  this.overlap = function(o1, o2) {
+    return !(o1.y + o1.h - 1 < o2.y || o1.y > o2.y + o2.h - 1 ||
+             o1.x + o1.w - 1 < o2.x || o1.x > o2.x + o2.w - 1);
   };
 
   // Find the first object that collides with obj
   // match against an optional type
-  this.collide = function (obj, type) {
-    return this.detect(function () {
+  this.collide = function(obj, type) {
+    return this.detect(function() {
       if (obj != this) {
         var col = (!type || this.type & type) && board.overlap(obj, this);
         return col ? this : false;
@@ -301,9 +271,9 @@ var GameBoard = function () {
   };
 };
 
-var Sprite = function () {};
+var Sprite = function() {};
 
-Sprite.prototype.setup = function (sprite, props) {
+Sprite.prototype.setup = function(sprite, props) {
   this.sprite = sprite;
   this.merge(props);
   this.frame = this.frame || 0;
@@ -311,7 +281,7 @@ Sprite.prototype.setup = function (sprite, props) {
   this.h = SpriteSheet.map[sprite].h;
 };
 
-Sprite.prototype.merge = function (props) {
+Sprite.prototype.merge = function(props) {
   if (props) {
     for (var prop in props) {
       this[prop] = props[prop];
@@ -319,15 +289,12 @@ Sprite.prototype.merge = function (props) {
   }
 };
 
-Sprite.prototype.draw = function (ctx) {
-  SpriteSheet.draw(ctx, this.sprite, this.x, this.y, this.frame);
-};
+Sprite.prototype.draw = function(
+    ctx) { SpriteSheet.draw(ctx, this.sprite, this.x, this.y, this.frame); };
 
-Sprite.prototype.hit = function (damage) {
-  this.board.remove(this);
-};
+Sprite.prototype.hit = function(damage) { this.board.remove(this); };
 
-var Level = function (levelData, callback) {
+var Level = function(levelData, callback) {
   this.levelData = [];
   for (var i = 0; i < levelData.length; i++) {
     this.levelData.push(Object.create(levelData[i]));
@@ -336,10 +303,8 @@ var Level = function (levelData, callback) {
   this.callback = callback;
 };
 
-Level.prototype.step = function (dt) {
-  var idx = 0,
-    remove = [],
-    curShip = null;
+Level.prototype.step = function(dt) {
+  var idx = 0, remove = [], curShip = null;
 
   // Update the current time offset
   this.t += dt * 1000;
@@ -352,8 +317,7 @@ Level.prototype.step = function (dt) {
       remove.push(curShip);
     } else if (curShip[0] < this.t) {
       // Get the enemy definition blueprint
-      var enemy = enemies[curShip[3]],
-        override = curShip[4];
+      var enemy = enemies[curShip[3]], override = curShip[4];
 
       // Add a new enemy with the blueprint and override
       this.board.add(new Enemy(enemy, override));
@@ -367,24 +331,26 @@ Level.prototype.step = function (dt) {
   // Remove any objects from the levelData that have passed
   for (var i = 0, len = remove.length; i < len; i++) {
     var remIdx = this.levelData.indexOf(remove[i]);
-    if (remIdx != -1) this.levelData.splice(remIdx, 1);
+    if (remIdx != -1)
+      this.levelData.splice(remIdx, 1);
   }
 
   // If there are no more enemies on the board or in
   // levelData, this level is done
   if (this.levelData.length === 0 && this.board.cnt[OBJECT_ENEMY] === 0) {
-    if (this.callback) this.callback();
+    if (this.callback)
+      this.callback();
   }
 };
 
-Level.prototype.draw = function (ctx) {};
+Level.prototype.draw = function(ctx) {};
 
-var TouchControls = function () {
+var TouchControls = function() {
   var gutterWidth = 10;
   var unitWidth = Game.width / 5;
   var blockWidth = unitWidth - gutterWidth;
 
-  this.drawSquare = function (ctx, x, y, txt, on) {
+  this.drawSquare = function(ctx, x, y, txt, on) {
     ctx.globalAlpha = on ? 0.9 : 0.6;
     ctx.fillStyle = "#CCC";
     ctx.fillRect(x, y, blockWidth, blockWidth);
@@ -395,33 +361,25 @@ var TouchControls = function () {
 
     var txtSize = ctx.measureText(txt);
 
-    ctx.fillText(
-      txt,
-      x + blockWidth / 2 - txtSize.width / 2,
-      y + (3 * blockWidth) / 4 + 5
-    );
+    ctx.fillText(txt, x + blockWidth / 2 - txtSize.width / 2,
+                 y + (3 * blockWidth) / 4 + 5);
   };
 
-  this.draw = function (ctx) {
+  this.draw = function(ctx) {
     ctx.save();
 
     var yLoc = Game.height - unitWidth;
     this.drawSquare(ctx, gutterWidth, yLoc, "\u25C0", Game.keys["left"]);
-    this.drawSquare(
-      ctx,
-      unitWidth + gutterWidth,
-      yLoc,
-      "\u25B6",
-      Game.keys["right"]
-    );
+    this.drawSquare(ctx, unitWidth + gutterWidth, yLoc, "\u25B6",
+                    Game.keys["right"]);
     this.drawSquare(ctx, 4 * unitWidth, yLoc, "A", Game.keys["fire"]);
 
     ctx.restore();
   };
 
-  this.step = function (dt) {};
+  this.step = function(dt) {};
 
-  this.trackTouch = function (e) {
+  this.trackTouch = function(e) {
     var touch, x;
 
     e.preventDefault();
@@ -454,37 +412,26 @@ var TouchControls = function () {
   Game.canvas.addEventListener("touchend", this.trackTouch, true);
 
   // For Android
-  Game.canvas.addEventListener(
-    "dblclick",
-    function (e) {
-      e.preventDefault();
-    },
-    true
-  );
-  Game.canvas.addEventListener(
-    "click",
-    function (e) {
-      e.preventDefault();
-    },
-    true
-  );
+  Game.canvas.addEventListener("dblclick", function(e) { e.preventDefault(); },
+                               true);
+  Game.canvas.addEventListener("click", function(e) { e.preventDefault(); },
+                               true);
 
   Game.playerOffset = unitWidth + 20;
 };
 
-var GamePoints = function () {
+var GamePoints = function() {
   Game.points = 0;
 
   var pointsLength = 8;
 
-  this.draw = function (ctx) {
+  this.draw = function(ctx) {
     ctx.save();
     ctx.font = "bold 18px arial";
     ctx.fillStyle = "#00FF00";
 
     var txt = "" + Game.points;
-    var i = pointsLength - txt.length,
-      zeros = "";
+    var i = pointsLength - txt.length, zeros = "";
     while (i-- > 0) {
       zeros += "0";
     }
@@ -493,5 +440,5 @@ var GamePoints = function () {
     ctx.restore();
   };
 
-  this.step = function (dt) {};
+  this.step = function(dt) {};
 };
